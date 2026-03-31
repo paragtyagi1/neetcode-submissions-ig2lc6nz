7cc30ea4e7766d362018951ee3d27378a28a2ef3
Parag Tyagi <tyagiparag60@gmail.com>
2026-03-31 19:04:14 +0530

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.js
@@ -0,0 +1,65 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        let heap = [];
+        let result = [];
+
+        for(let [x,y] of points){
+          heap.push([Math.sqrt((x * x) + (y * y)),x,y]);
+        }
+
+        this.buildMinHeap(heap);
+
+        for(let i=0;i<k;i++){
+            let [dist,x,y] = this.extractMin(heap)
+            result.push([x,y]);
+        }
+
+        return result;
+    }
+
+    buildMinHeap(heap){
+     let n = heap.length;
+     for(let i = Math.floor(n/2)-1;i>=0;i--){
+         this.heapify(heap,i)
+     }
+    }
+
+    heapify(heap, i) {
+    let n = heap.length;
+
+    while (true) {
+        let smallest = i;
+        let left = 2 * i + 1;
+        let right = 2 * i + 2;
+
+        if (left < n && heap[left][0] < heap[smallest][0]) {
+            smallest = left;
+        }
+
+        if (right < n && heap[right][0] < heap[smallest][0]) {
+            smallest = right;
+        }
+
+        if (smallest === i) break;
+
+        [heap[i], heap[smallest]] = [heap[smallest], heap[i]];
+        i = smallest;
+    }
+}
+
+    extractMin(heap){
+        let min = heap[0];
+    
+        if(heap.length>0){
+             heap[0] = heap.pop();
+            this.heapify(heap,0);
+        }
+
+        return min;
+    }
+}
