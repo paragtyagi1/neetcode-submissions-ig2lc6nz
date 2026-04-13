429e078b726158e29d15a4510fc46c0e0c9774b6
Parag Tyagi <tyagiparag60@gmail.com>
2026-04-13 15:00:17 +0530

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+
+        let slow = nums[0];
+        let fast = nums[nums[0]];
+
+        while (slow !== fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = 0;
+
+        while (slow !== fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
