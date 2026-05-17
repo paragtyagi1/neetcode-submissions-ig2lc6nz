1123b6d0454e2128835d90992b1a7de0b0305f1e
Parag Tyagi <tyagiparag60@gmail.com>
2026-05-17 20:24:52 +0530

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        let wordSet = new Set();
+
+        for (let word of wordDict) {
+            wordSet.add(word);
+        }
+
+        let memo = new Map();
+
+        let solve = (word) => {
+            if (word.length === 0) {
+                return true;
+            }
+
+            if(memo.has(word)) return memo.get(word);
+
+            for (let i = 0; i < word.length; i++) {
+                let substring = word.slice(0, i+1);
+                if (wordSet.has(substring)) {
+                    if(memo.has(word.slice(i + 1))) return memo.get(word.slice(i + 1));
+                    if (solve(word.slice(i + 1))) {
+                        return true;
+                    }
+                    memo.set(word.slice(i + 1),false);
+                }
+            }
+
+            return false;
+        };
+
+        return solve(s);
+    }
+}
