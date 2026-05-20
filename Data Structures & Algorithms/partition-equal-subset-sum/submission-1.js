b9effb3cf366320721062e1bbd65c7282d72a06f
Parag Tyagi <tyagiparag60@gmail.com>
2026-05-20 12:37:48 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        let sum = nums.reduce((a, b) => a + b, 0);
+        if (sum % 2 !== 0) {
+            return false;
+        }
+        const target = sum / 2;
+        const n = nums.length;
+
+        const dp = Array.from(Array(n + 1), () => Array(target + 1).fill(false));
+
+        for (let i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (let i = 1; i <= n; i++) {
+            for (let sum = 1; sum <= target; sum++) {
+                if (nums[i - 1] <= sum) {
+                    dp[i][sum] = dp[i - 1][sum] || dp[i - 1][sum - nums[i - 1]];
+                } else {
+                    dp[i][sum] = dp[i - 1][sum];
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
