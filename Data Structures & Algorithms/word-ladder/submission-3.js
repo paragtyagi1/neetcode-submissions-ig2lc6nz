894f49022c58076497ba2a8041490ebf79d171bf
Parag Tyagi <tyagiparag60@gmail.com>
2026-05-21 21:16:45 +0530

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {string} beginWord
+     * @param {string} endWord
+     * @param {string[]} wordList
+     * @return {number}
+     */
+    ladderLength(beginWord, endWord, wordList) {
+        let wordlength = beginWord.length;
+        let graph = new Map();
+
+        if(!wordList.includes(endWord)) return 0 // if no endWord return 0
+      
+        for(let word of wordList){
+          for(let i=0; i<wordlength ;i++){
+                let pattern = word.slice(0,i)+"*"+word.slice(i+1);
+
+                if(!graph.has(pattern)){
+                    graph.set(pattern,[]);
+                }
+
+                graph.get(pattern).push(word);
+          }
+        }
+
+        // Check via BFS for every word (generted patterns have the end word,
+        // if yes then push its neighbours to queue and perform same operration)
+
+        let queue = [[beginWord,1]]; // word and steps
+        let visited = new Set();
+        visited.add(beginWord);
+
+        while(queue.length > 0){
+          let [word,steps] = queue.shift();
+
+          for(let i = 0; i<wordlength; i++){
+            let pattern = word.slice(0,i)+"*"+word.slice(i+1);
+
+            let neighbors = graph.get(pattern);
+
+            if(neighbors && neighbors.length){
+               for(let neighbor of neighbors){
+                if(neighbor === endWord){
+                    return steps + 1;
+                }
+                if(!visited.has(neighbor)){
+                    queue.push([neighbor,steps+1]);
+                    visited.add(neighbor);
+                }
+               }
+            }
+          }
+        }
+
+        return 0
+    }
+}
