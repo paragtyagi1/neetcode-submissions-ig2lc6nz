0b7357ff241c1df20838da16bac11556b8e86683
Parag Tyagi <tyagiparag60@gmail.com>
2026-05-31 13:24:10 +0530

    Add: cheapest-flight-path - submission-2

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-2.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} flights
+     * @param {number} src
+     * @param {number} dst
+     * @param {number} k
+     * @return {number}
+     */
+    findCheapestPrice(n, flights, src, dst, k) {
+        let graph = new Map();
+
+        for (let i = 0; i < n; i++) {
+            graph.set(i, []);
+        }
+
+        for (let [source, destination, price] of flights) {
+            graph.get(source).push([destination, price]);
+        }
+
+        let queue = [];
+        let prices = new Array(n).fill(Infinity);
+        prices[src] = 0 ; // important step..
+
+        queue.push([src, 0, 0]);
+
+        while (queue.length > 0) {
+            let [node, cost, stops] = queue.shift();
+            
+            if (stops > k) continue;
+
+            //edge relaxation..
+            for (let [neighbor, price] of graph.get(node)) {
+                let newCost = cost + price;
+                if ( newCost < prices[neighbor]) {
+                    prices[neighbor] = newCost;
+                    queue.push([neighbor,newCost, stops + 1]);
+                }
+            }
+        }
+
+        return prices[dst] === Infinity ? -1 : prices[dst];
+    }
+}
