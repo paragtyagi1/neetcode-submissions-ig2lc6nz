583d153175b49bb8144f776c769b88a4216c8590
Parag Tyagi <tyagiparag60@gmail.com>
2026-06-02 19:30:01 +0530

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+        
+        //let dp = new Araay({length:n},()=> new Array(n));
+        let memo = new Map();
+
+        let solve = (i, j) => {
+            let key = `${i},${j}`;
+
+            if (i === text1.length || j === text2.length) {
+                return 0;
+            }
+
+            if (memo.has(key)) return memo.get(key);
+
+            let ans;
+
+            if (text1[i] === text2[j]) {
+                ans = 1 + solve(i + 1, j + 1);
+            } else {
+                ans = Math.max(solve(i, j + 1), solve(i + 1, j));
+            }
+
+            memo.set(key, ans);
+            return ans;
+        };
+        return solve(0, 0);
+    }
+}
