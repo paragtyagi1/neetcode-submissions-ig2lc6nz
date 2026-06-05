f169b65f64fb5bc7796d53f65934dcb2cd26b156
Parag Tyagi <tyagiparag60@gmail.com>
2026-06-05 19:36:49 +0530

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    numDistinct(s, t) {
+        const m = s.length;
+        const n = t.length;
+
+        const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+        for (let i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (let i = m - 1; i >= 0; i--) {
+            for (let j = n - 1; j >= 0; j--) {
+                if (s[i] === t[j]) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
