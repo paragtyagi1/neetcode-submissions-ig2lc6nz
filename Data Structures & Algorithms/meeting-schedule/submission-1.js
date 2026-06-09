13db3e6a5e052fb9105b4b38883f90b9c8d270e3
Parag Tyagi <tyagiparag60@gmail.com>
2026-06-09 18:20:13 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,13 @@
+class Solution {
+    canAttendMeetings(intervals) {
+        intervals.sort((a, b) => a.start - b.start);
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
